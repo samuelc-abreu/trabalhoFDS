28d283344c0e0de91c7dcf1349c198f4ce9b230a
samuelc-abreu <abreu.samuel@hotmail.com>
2025-05-16 13:24:17 -0300

    adicionado funções para o botão

---
+++ script.js
@@ -0,0 +1,8 @@
+function mensagem() {
+   const i = document.getElementById("motivacional");
+   if(i.style.display === "none" ){
+    i.style.display = "block";
+   }else{
+    i.style.display = "none";
+   }
+}
